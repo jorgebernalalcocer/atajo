f036503faa1cb8b809ceff20b740f4c2205e9dbd
jorgebernalalcocer <j.90.bernal@gmail.com>
2022-02-14 18:12:36 +0100

    sd

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -8,9 +8,8 @@ package gps;
 
 public class Bicicleta {
     public Bicicleta(){
-        int ruedas = 4;
-        boolean luces = false;
-        String marca = "Orbea";
+     
+      
     }
     
     public static String rutaCorta(int [] camino1, int [] camino2, int [] camino3)
@@ -52,10 +51,10 @@ public class Bicicleta {
     }
 
     public static void main(String[] args) {
-        Bicicleta bici = new Bicicleta();
-        int[] camino1 = {12,34,21,46,25};
-        int[] camino2 = {24,1,5,64,10,15,21};
-        int[] camino3 = {1,5,6,7,8,10,20,46,104};
+        
+        int[] camino1 = {12,34,21,};
+        int[] camino2 = {24,1,5,64,10,15,221};
+        int[] camino3 = {1,5,6,7,8,10,20,46,604};
         System.out.println("El camino más corto es:"+Bicicleta.rutaCorta(camino1,camino2,camino3)+".");
     }
 }
